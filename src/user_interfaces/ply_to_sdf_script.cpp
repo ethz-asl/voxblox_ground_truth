d5db273278e9af7f55418c79ec08830725458da2
Victor Reijgwart <victor_reijgwart@hotmail.com>
2019-05-22 22:19:30 +0200

    Expose all params as cmd line args and added sample .ply file

--- src/user_interfaces/ply_to_sdf_script.cpp
+++ src/user_interfaces/ply_to_sdf_script.cpp
@@ -19,9 +19,6 @@
 
 int main(int argc, char *argv[]) {
   using PointcloudMsg = pcl::PointCloud<pcl::PointXYZI>;
-  // TODO(victorr): Implement args format check
-  // TODO(victorr): Implement --help
-
   /* Advertise the debugging ROS topic */
   // Register with ROS
   ros::init(argc, argv, "voxblox_ground_truth");
@@ -30,32 +27,33 @@ int main(int argc, char *argv[]) {
   voxblox_ground_truth::SdfVisualizer sdf_visualizer(nh_private);
 
   /* Process the command line arguments */
-  if (argc != 11) {
+  if (argc != 12) {
     const std::string executable_path(argv[0]);
     const std::string executable_filename =
         executable_path.substr(executable_path.find_last_of('/') + 1);
-    std::cout << "Usage:\n"
-              << "./" << executable_filename << "  "
-              << "[ply_file_path] [voxel_size] [scale_factor] "
-                 "[X] [Y] [Z] [Qx] [Qy] [Qz] [Qw]"
-              << std::endl;
+    ROS_INFO_STREAM("Usage:\n"
+                    << "./" << executable_filename << " "
+                    << "[ply_input_file_path] [tsdf_output_file_path] "
+                       "[voxel_size] [scale_factor] "
+                       "[X] [Y] [Z] [Qx] [Qy] [Qz] [Qw]");
     return -1;
   }
-  std::string ply_filepath(argv[1]);
+  std::string ply_input_filepath(argv[1]);
+  std::string tsdf_output_filepath(argv[2]);
   float voxel_size;
-  std::stringstream(argv[2]) >> voxel_size;
+  std::stringstream(argv[3]) >> voxel_size;
   // Transformation and scaling from mesh to world frame
   double scale_factor;
-  std::stringstream(argv[3]) >> scale_factor;
+  std::stringstream(argv[4]) >> scale_factor;
   voxblox::FloatingPoint x, y, z;
-  std::stringstream(argv[4]) >> x;
-  std::stringstream(argv[5]) >> y;
-  std::stringstream(argv[6]) >> z;
+  std::stringstream(argv[5]) >> x;
+  std::stringstream(argv[6]) >> y;
+  std::stringstream(argv[7]) >> z;
   voxblox::FloatingPoint qx, qy, qz, qw;
-  std::stringstream(argv[7]) >> qx;
-  std::stringstream(argv[8]) >> qy;
-  std::stringstream(argv[9]) >> qz;
-  std::stringstream(argv[10]) >> qw;
+  std::stringstream(argv[8]) >> qx;
+  std::stringstream(argv[9]) >> qy;
+  std::stringstream(argv[10]) >> qz;
+  std::stringstream(argv[11]) >> qw;
   voxblox::Transformation transform;
   transform.getPosition() = voxblox::Transformation::Position(x, y, z);
   transform.getRotation().setValues(qx, qy, qz, qw);
@@ -65,22 +63,21 @@ int main(int argc, char *argv[]) {
   // Write the transformation params to cout for debugging
   Eigen::IOFormat ioformat(Eigen::StreamPrecision, Eigen::DontAlignCols, "; ",
                            "; ", "", "", "", "");
-  std::cout << "Will apply transformation:\n"
-            << "- scaling: " << scale_factor << "\n"
-            << "- translation: " << transform.getPosition().format(ioformat)
-            << "\n"
-            << "- rotation: "
-            << transform.getRotation().vector().format(ioformat) << "\n";
+  ROS_INFO_STREAM(
+      "Will apply transformation:\n"
+      << "- scaling: " << scale_factor << "\n"
+      << "- translation: " << transform.getPosition().format(ioformat) << "\n"
+      << "- rotation: " << transform.getRotation().vector().format(ioformat));
 
   /* Load the PLY file */
   pcl::PolygonMesh mesh;
-  std::cout << "Importing .ply file: " << ply_filepath << std::endl;
-  pcl::io::loadPLYFile(ply_filepath, mesh);
+  ROS_INFO_STREAM("Importing .ply file: " << ply_input_filepath);
+  pcl::io::loadPLYFile(ply_input_filepath, mesh);
   pcl::PointCloud<pcl::PointXYZ> vertex_coordinates;
   pcl::fromPCLPointCloud2(mesh.cloud, vertex_coordinates);
 
   // Initialize the SDF creator
-  std::cout << "Generating the TSDF" << std::endl;
+  ROS_INFO("Generating the TSDF");
   voxblox::TsdfMap::Config map_config;
   map_config.tsdf_voxel_size = voxel_size;
   voxblox_ground_truth::SdfCreator sdf_creator(map_config);
@@ -104,7 +101,7 @@ int main(int argc, char *argv[]) {
 
     // Exit if CTRL+C was pressed
     if (!ros::ok()) {
-      std::cout << "\nShutting down..." << std::endl;
+      ROS_INFO("Shutting down...");
       return -1;
     }
 
@@ -125,21 +122,26 @@ int main(int argc, char *argv[]) {
     // Update the SDF with the new triangle
     sdf_creator.integrateTriangle(triangle_vertices);
   }
-  std::cout << "\nDistance field building complete." << std::endl;
+  ROS_INFO("Distance field building complete.");
 
   /* Publish debugging visuals */
   bool publish_debug_visuals = true;
   nh_private.param("publish_visuals", publish_debug_visuals,
                    publish_debug_visuals);
   if (publish_debug_visuals) {
-    std::cout << "Publishing visuals" << std::endl;
+    ROS_INFO("Publishing visuals");
     sdf_visualizer.publishTsdfVisuals(
         sdf_creator.getTsdfMap().getTsdfLayer());
     sdf_visualizer.publishIntersectionVisuals(
         sdf_creator.getIntersectionLayer());
   }
 
-  std::cout << "Done" << std::endl;
+  // Save the TSDF to a file
+  ROS_INFO_STREAM("Saving TSDF to file: " << tsdf_output_filepath);
+  sdf_creator.getTsdfMap().getTsdfLayer().saveToFile(tsdf_output_filepath,
+                                                     true);
+
+  ROS_INFO("Done");
   ros::spin();
 
   return 0;
